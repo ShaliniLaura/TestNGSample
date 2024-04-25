2271106b42b2af6dc4c5cccb0fe74746a8778cf4
ShaliniLaura <emailid>
2024-04-25 07:23:49 +0000

    TestNG-Day-6-Commit

---
+++ src/test/java/testScripts/GooglePageTest.java
@@ -0,0 +1,22 @@
+package testScripts;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class GooglePageTest {
+  @Test
+  public void javaSearchTest() {
+	  
+	  WebDriver driver = new ChromeDriver();
+	  driver.manage().window().maximize();
+	  driver.get("https://www.google.com/");
+	  WebElement srcbox=driver.findElement(By.className("gLFyf"));
+	  srcbox.sendKeys("Java Tutorial");
+	  srcbox.submit();
+	  
+	  
+  }
+}
